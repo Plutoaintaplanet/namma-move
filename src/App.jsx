491a49abf91eef451ee7c590a76ea600ce96c9fa
Aakash Raghu <aakashraghu13@gmail.com>
2026-03-12 23:40:22 +0530

    fix: Import useMemo in App.jsx to resolve build error

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useCallback, useRef } from "react";
+import { useEffect, useState, useCallback, useRef, useMemo } from "react";
 import DualMapPicker from "./DualMapPicker";
 import NewsPage from "./NewsPage";
 import SchedulesPage from "./SchedulesPage";
